50246c185aac02e0987c62a7e00ffd77ca524e5e
최우석 <nick501@naver.com>
2025-09-26 23:50:43 +0900

    [공통 컴포넌트]Toast, Reaction 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css';
-import FeedPage from './pages/FeedPage';
+import GlobalStyle from './components/GlobalStyle';
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <p>이곳은 메인 화면입니다.</p>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import FeedPage from './pages/FeedPage.jsx';
+import Reaction from './components/Reaction/Reaction.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <FeedPage />
+    {/* <FeedPage /> */}
+    <Reaction />
   </StrictMode>
 );
