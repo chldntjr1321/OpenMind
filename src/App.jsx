0c61d61b7c360bd84cb2e13fa7d324da30738d62
glowhyeon <shyeon710@gmail.com>
2025-09-26 15:32:48 +0900

    질문 모달창 HTML 작업

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
+import Modal from './pages/Modal'
 
 function App() {
-  return (
-    <>
-      <p>이곳은 메인 화면입니다.</p>
-    </>
-  );
+  return <Modal />;
+  // return (
+  //   <>
+  //     <p>이곳은 메인 화면입니다.</p>
+  //   </>
+  // );
 }
 
 export default App;
