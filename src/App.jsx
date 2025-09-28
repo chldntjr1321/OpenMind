d83b31a2a273a2aed6a00c141c759d983451ff3a
최우석 <nick501@naver.com>
2025-09-28 19:25:21 +0900

    [공통 컴포넌트] Edit 메뉴 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import Badge from './components/Badge/Badge.jsx';
 import Dropdown from './components/Dropdown/Dropdown.jsx';
+import Edit from './components/Edit/Edit.jsx';
 import GlobalStyle from './components/GlobalStyle';
 import Reaction from './components/Reaction/Reaction.jsx';
 import UserCard from './components/UserCard/UserCard.jsx';
+import Toast from './components/Toast/Toast.jsx';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <p>이곳은 메인 화면입니다.</p>
+      <Toast />
       <Reaction />
       <Badge />
       <UserCard />
       <Dropdown />
+      <Edit />
     </>
   );
 }
