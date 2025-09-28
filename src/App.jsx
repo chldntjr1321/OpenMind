115836dd57318a74007627638295cad8b5cc9df6
최우석 <nick501@naver.com>
2025-09-28 11:09:56 +0900

    [공통 컴포넌트] Badge 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Badge from './components/Badge/Badge.jsx';
 import GlobalStyle from './components/GlobalStyle';
+import Reaction from './components/Reaction/Reaction.jsx';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <p>이곳은 메인 화면입니다.</p>
+      <Reaction />
+      <Badge />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import FeedPage from './pages/FeedPage.jsx';
-import Reaction from './components/Reaction/Reaction.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    {/* <FeedPage /> */}
-    <Reaction />
+    <App />
   </StrictMode>
 );
