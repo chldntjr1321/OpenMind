6f990e83e3f385556f73aed84c8cc27f24df2a59
glowhyeon <shyeon710@gmail.com>
2025-09-29 14:48:05 +0900

    InputField 제작

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import './App.css';
 import Modal from './pages/Modal'
 
 function App() {
-  return <Modal />;
-  // return (
-  //   <>
-  //     <p>이곳은 메인 화면입니다.</p>
-  //   </>
-  // );
+  return (
+    <>
+      <p>이곳은 메인 화면입니다.</p>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import InputTextArea from './components/InputTextArea/InputTextArea.jsx'
+import InputField from './components/InputField/InputField.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <InputTextArea /> */}
+    <InputField />
   </StrictMode>,
 )
