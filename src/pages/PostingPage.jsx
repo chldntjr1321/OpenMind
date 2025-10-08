98e7b2378f638fb48142b474b8c9aeee491adb77
glowhyeon <shyeon710@gmail.com>
2025-10-08 16:53:01 +0900

    [Page] 개별 피드 레이아웃 수정 및 추가 제작 (모바일 화면)

--- src/pages/PostingPage.jsx
+++ src/pages/PostingPage.jsx
@@ -6,39 +6,52 @@ const PostingHeader = styled.div`
   position: relative;
   flex-direction: column;
   align-items: center;
-  gap: 12px;
+  justify-content: flex-start;
+  width: 100%;
+  min-height: 369px;
+  margin: 0 auto;
+  @media (max-width: 375px) {
+    min-height: 299px;
+  }
 `;
 const PostingHeaderImage = styled.div`
   background-color: #999999;
   width: 100%;
   height: 234px;
-  position: absolute;
-  max-width: 1200px;
-  min-height: 177px;
-  margin: 0 auto;
-`;
-const PostingLogo = styled.div`
-  background-color: #666666;
-  position: relative;
-  margin-top: 50px;
-  width: 170px;
-  height: 67px;
-  min-width: 124px;
-  min-height: 49px;
-`;
+  object-fit: cover;
+  object-position: center;
+  `;
 const ProfileArea = styled.div`
   background-color: #666666;
   display: flex;
   flex-direction: column;
   align-items: center;
-  position: relative;
+  position: absolute;
+  top: 50px;
+  min-height: auto;
   gap: 12px;
-  height: 240px;
+  @media (max-width: 375px) {
+    top: 40px;
+  }
+`;
+const PostingLogo = styled.div`
+  background-color: #777777;
+  position: relative;
+  width: 170px;
+  height: 67px;
+  @media (max-width: 375px) {
+    width: 124px;
+    height: 49px;
+  }
 `;
 const ProfileImg = styled.div`
   background-color: #888888;
   width: 136px;
   height: 136px;
+  @media (max-width: 375px) {
+    width: 104px;
+    height: 104px;
+  }
 `;
 const ProfileUsername = styled.div`
   background-color: #888888;
@@ -48,6 +61,10 @@ const ProfileUsername = styled.div`
   font-size: 32px;
   line-height: 40px;
   letter-spacing: 0px;
+  @media (max-width: 375px) {
+    font-size: 24px;
+    line-height: 30px;
+  }
 `;
 const ShareIconArea = styled.div`
   background-color: #888888;
@@ -63,8 +80,7 @@ const PostingBody = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 54px;
-  padding-bottom: 166px;
+  padding: 54px 24px 166px;
 `;
 const PostingArea = styled.div`
   background-color: #f5f1ee;
@@ -72,19 +88,26 @@ const PostingArea = styled.div`
   flex-direction: column;
   align-items: center;
   position: relative;
+  width: 100%;
   max-width: 716px;
   height: auto;
   border: 1px solid #c7bbb5;
   border-radius: 16px;
   padding: 16px;
   gap: 16px;
-  & div {
+  .questionCount {
     color: #542f1a;
     font-family: Actor;
     font-weight: 400;
     font-size: 20px;
     line-height: 25px;
     letter-spacing: 0px;
+    @media (max-width: 375px) {
+      font-family: Actor;
+      font-weight: 400;
+      font-size: 18px;
+      line-height: 24px;
+    }
   }
 `;
 const FeedCard = styled.div`
@@ -92,29 +115,62 @@ const FeedCard = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 684px;
+  width: 100%;
+  max-width: 684px;
   height: auto;
   border-radius: 16px;
-  padding: 32px 16px;
+  padding: 32px;
   gap: 32px;
+  @media (max-width: 375px) {
+    max-width: 295px;
+    padding: 24px 16px;
+    gap: 24px;
+  }
 `;
 const FeedBadge = styled.div`
   background-color: #999999;
-  width: 620px;
+  width: 100%;
   height: 26px;
+  @media (max-width: 375px) {
+    width: 100%;
+  }
 `;
 const QuestionFeed = styled.div`
   background-color: orange;
   display: flex;
   flex-direction: column;
-  width: 620px;
+  width:100%;
   height: auto;
+  .question {
+    font-family: Actor;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 24px;
+    letter-spacing: 0px;
+    @media (max-width: 375px) {
+      font-size: 16px;
+      line-height: 22px;
+    }
+  }
+`;
+const UpdatedTime = styled.div`
+  color: #818181;
+  display: inline-flex;
+  font-family: Pretendard;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 18px;
+  letter-spacing: 0px;
+  @media (max-width: 375px) {
+    font-size: 14px;
+    line-height: 18px;
+  }
 `;
 const AnswerFeed = styled.div`
   background-color: #999999;
   display: flex;
   flex-direction: row;
-  width: 620px;
+  width: 100%;
   height: auto;
   gap: 4px 12px;
 `;
@@ -131,22 +187,45 @@ const AnswerProfileImg = styled.div`
   background-color: #666666;
   width: 48px;
   height: 48px;
+  flex-shrink: 0;
 `;
 const AnswerUsername = styled.div`
   background-color: orange;
-  width: 560px;
-  height: 24px;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: auto;
+  gap: 12px;
+  & {
+    font-family: Actor;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 24px;
+    letter-spacing: 0px;
+    @media (max-width: 375px) {
+      font-size: 14px;
+      line-height: 18px;
+    }
+  }
 `;
 const AnswerContents = styled.div`
   background-color: orange;
-  width: 560px;
+  width: 100%;
   height: auto;
+  & {
+    font-family: Pretendard;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 22px;
+    letter-spacing: 0px;
+  }
+  word-break: break-word;
 `;
 const AnswerReaction = styled.div`
   background-color: #999999;
   display: flex;
   flex-direction: column;
-  width: 620px;
+  width: 100%;
   height: 43px;
 `;
 const FloatingBtn = styled.div`
@@ -160,33 +239,38 @@ function PostingPage() {
   return (
     <>
       <PostingHeader>
-        <PostingHeaderImage>
-          메인 이미지
-        </PostingHeaderImage>
-        <PostingLogo />
+        <PostingHeaderImage />
         <ProfileArea>
+          <PostingLogo />
           <ProfileImg />
           <ProfileUsername>
-            아초는 고양이
+            아초는고양이
           </ProfileUsername>
           <ShareIconArea />
         </ProfileArea>
       </PostingHeader>
       <PostingBody>
         <PostingArea>
-          <div>3개의 질문이 있습니다</div>
+          <div className="questionCount">3개의 질문이 있습니다</div>
           <FeedCard>
             <FeedBadge />
             <QuestionFeed>
-              <div>질문 · 2주전</div>
-              <div>좋아하는 동물은?</div>
+              <div>
+                <UpdatedTime>질문 · 2주전</UpdatedTime>
+              </div>
+              <div className="question">좋아하는 동물은?</div>
             </QuestionFeed>
             <AnswerFeed>
               <AnswerLeft>
                 <AnswerProfileImg />
               </AnswerLeft>
               <AnswerRight>
-                <AnswerUsername />
+                <AnswerUsername>
+                  <span>아초는고양이</span>
+                  <div>
+                    <UpdatedTime>2주전</UpdatedTime>
+                  </div>
+                </AnswerUsername>
                 <AnswerContents>
                   <div>그들을 불러 귀는 이상의 오직 피고, 가슴이 이상, 못할 봄바람이다. 찾아다녀도, 전인 방황하였으며, 대한 바이며, 이것이야말로 가치를 청춘의 따뜻한 그리하였는가? 몸이 열락의 청춘의 때문이다. 천고에 피어나는 간에 밝은 이상, 인생의 만물은 피다. 대중을 이성은 방황하여도, 그리하였는가? 크고 평화스러운 품에 방황하였으며, 말이다. 이상은 들어 예수는 크고 긴지라 역사를 피다. 얼음에 있음으로써 꽃 보배를 곧 가는 교향악이다. 우는 새 예가 우리의 것은 피다. 피가 그것을 어디 앞이 기쁘며, 이상의 열락의 위하여서 끝까지 것이다. 있는 봄바람을 방황하여도, 우리의 것은 작고 아니한 영원히 듣기만 운다.</div>
                 </AnswerContents>
